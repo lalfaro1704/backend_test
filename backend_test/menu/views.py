ef56f1042d44de704e2317c54cb00673990e61f9
Luis Alejandro Alfaro Gonzalez <luipi.alfaro@gmail.com>
2018-12-03 16:48:09 -0300

    filtro y ordering API ingrediente

--- backend_test/menu/views.py
+++ backend_test/menu/views.py
@@ -1,6 +1,10 @@
+# standard libreary
+import coreapi
+
 # 3rd-party
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.filters import BaseFilterBackend, OrderingFilter
 
 # my models here
 from backend_test.menu.models import (Ingredient, Preparation)
@@ -11,6 +15,28 @@ from .serializers import (IngredientSerializer, PreparationSerializer)
 # Create your views here.
 
 
+class IngredientFilterBackend(BaseFilterBackend):
+    """
+    Filter for ingredient API.
+    """
+    def get_schema_fields(self, view):
+        return [
+            coreapi.Field(
+                name='preparation',
+                location='query',
+                required=False,
+                type='str',
+                description='Filter ingredients by preparation.',
+            ),
+        ]
+
+    def filter_queryset(self, request, queryset, view):
+        preparation = request.GET.get('preparation', None)
+        if preparation:
+            return queryset.filter(preparation__id=preparation)
+        return queryset
+
+
 class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     list:
@@ -18,6 +44,8 @@ class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    filter_backends = [OrderingFilter, IngredientFilterBackend]
+    ordering_fields = ('name', '-name',)
 
 
 class PreparationViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
