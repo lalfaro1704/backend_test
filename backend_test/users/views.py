60abbeb823fb6911e1a2ec5c825656557e1db4d3
Luis Alejandro Alfaro Gonzalez <luipi.alfaro@gmail.com>
2018-12-03 16:47:01 -0300

    borrando funcionalidades innecesarios de aplicacion usuarios

--- backend_test/users/views.py
+++ backend_test/users/views.py
@@ -24,29 +24,3 @@ class UserListView(LoginRequiredMixin, ListView):
 
 
 user_list_view = UserListView.as_view()
-
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    model = User
-    fields = ["name"]
-
-    def get_success_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
-
-    def get_object(self):
-        return User.objects.get(username=self.request.user.username)
-
-
-user_update_view = UserUpdateView.as_view()
-
-
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-
-    permanent = False
-
-    def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
-
-
-user_redirect_view = UserRedirectView.as_view()
