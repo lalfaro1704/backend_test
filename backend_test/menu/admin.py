aa589339253f31b4f7c89af66aaf5acd27ef7240
Luis Alejandro Alfaro Gonzalez <luipi.alfaro@gmail.com>
2018-12-04 00:03:31 -0300

    admin para ingrediente, preparacion, almuerzo, menu

--- backend_test/menu/admin.py
+++ backend_test/menu/admin.py
@@ -1,3 +1,39 @@
+# standard library
 from django.contrib import admin
 
 # Register your models here.
+from .models import (Ingredient, Preparation, Lunch, Menu)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active']
+    search_fields = ('name',)
+    list_filter = ['is_active', ]
+
+
+class PreparationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active']
+    search_fields = ('name',)
+    list_filter = ['is_active', ]
+    filter_horizontal = ('recipe',)
+
+
+class LunchAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active']
+    search_fields = ('name',)
+    list_filter = ['is_active', ]
+    filter_horizontal = ('preparations',)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name', 'date', 'is_active']
+    ordering = ('date',)
+    search_fields = ('name',)
+    list_filter = ['is_active', ]
+    filter_horizontal = ('lunches',)
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Preparation, PreparationAdmin)
+admin.site.register(Lunch, LunchAdmin)
+admin.site.register(Menu, MenuAdmin)
--- backend_test/menu/urls.py
+++ backend_test/menu/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url, include
 
 from rest_framework import routers
 
-from .views import (IngredientViewSet, PreparationViewSet, LunchViewSet)
+from .views import (IngredientViewSet, PreparationViewSet, LunchViewSet, MenuViewSet)
 
 
 router = routers.DefaultRouter()
 router.register(r'ingredient', IngredientViewSet)
 router.register(r'preparation', PreparationViewSet)
 router.register(r'lunch', LunchViewSet)
+router.register(r'menu', MenuViewSet)
 
 
 urlpatterns = [
