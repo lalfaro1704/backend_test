570b8aecca643e4c2d99b4d560b959f99a0b7f88
Luis Alejandro Alfaro Gonzalez <luipi.alfaro@gmail.com>
2018-12-10 17:58:26 -0300

    prueba unitaria de modelo Ingredient

--- backend_test/menu/tests.py
+++ backend_test/menu/tests.py
@@ -1,3 +1,18 @@
+# Django
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your tests here.
+# my models here
+from .models import (Ingredient, Preparation, Lunch, Menu)
+
+
+class IngredientTest(TestCase):
+
+    def create_ingredient(self, name="Ingredient Test", is_active=True):
+        return Ingredient.objects.create(name=name, is_active=is_active)
+
+    def test_ingredient_creation(self):
+        ingredient = self.create_ingredient()
+        self.assertTrue(isinstance(ingredient, Ingredient))
+        self.assertEqual(ingredient.__str__(), ingredient.name)
