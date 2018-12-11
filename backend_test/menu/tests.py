46f17ec6939dd434c065c576e11dbe00d655d820
Luis Alejandro Alfaro Gonzalez <luipi.alfaro@gmail.com>
2018-12-10 22:39:46 -0300

    prueba unitaria para API Menu

--- backend_test/menu/tests.py
+++ backend_test/menu/tests.py
@@ -73,3 +73,33 @@ class GetAllPreparations(TestCase):
 
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetAllMenus(TestCase):
+    """
+    Test API for Menus.
+    """
+    def setUp(self):
+        i1 = Ingredient.objects.create(name="Ingredient 1", is_active=True)
+        i2 = Ingredient.objects.create(name="Ingredient 2", is_active=True)
+        i3 = Ingredient.objects.create(name="Ingredient 3", is_active=True)
+        i4 = Ingredient.objects.create(name="Ingredient 4", is_active=True)
+        p1 = Preparation.objects.create(name="Preparation 1", is_active=True)
+        p2 = Preparation.objects.create(name="Preparation 2", is_active=True)
+        p1.recipe.add(i1, i2)
+        p2.recipe.add(i3, i4)
+        l1 = Lunch.objects.create(name="Lunch 1", is_active=True)
+        l2 = Lunch.objects.create(name="Lunch 2", is_active=True)
+        l1.preparations.add(p1)
+        l2.preparations.add(p2)
+        menu = Menu.objects.create(name="Menu 1", date=datetime.now())
+        menu.lunches.add(l1, l2)
+
+    def test_get_all_ingredients(self):
+        response = client.get('/menu/api/menu/')  # get API response
+
+        menus = Menu.objects.all()  # get data from db
+        serializer = MenuSerializer(menus, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
